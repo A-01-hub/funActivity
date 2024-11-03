b6c5da65bb2dfa2d2aa3974a7232c4aa3400b727
Aditya Suhane <adityasuhane01@gmail.com>
2024-11-04 02:38:15 +0530

    some new chages in index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const age =24;
 const name1 ='Aditya Suhane' 
 const dob="06/09/2004 "
 const other_name="Vansh"
- function sayName(){
+ function sayName(name){
     console.log("My name is "+name)
  }
  function newfn(){
