e33d257638446f48b227cad0cc7c0d9b2e03d68d
Aditya Suhane <adityasuhane01@gmail.com>
2024-11-05 13:43:57 +0530

    replace concat with template literal

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ const name1 ='Aditya Suhane'
 const dob="06/09/2004 "
 const other_name="Vansh"
  function sayName(name){
-    console.log("My name is "+name)
+    console.log(`My name is ${name}`)
  }
